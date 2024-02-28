d3c93e1a44e36e5e3f409aa6fb15beb1c0a70755
Gabriel-barbos <126466365+Gabriel-barbos@users.noreply.github.com>
2024-02-28 11:24:55 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException,Request
+from starlette.datastructures import Headers, QueryParams
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+app.mount("/web", StaticFiles(directory="frontEnd"), name="web")
+@app.get("/web")
+async def root():
+ return FileResponse('frontEnd/index.html')
+
+
+@app.get("/info")
+async def request_info(request: Request):
+ headers = request.headers
+ query_params = request.query_params
+ headers_dict = dict(headers)
+ query_params_dict = dict(query_params)
+ return {
+    "headers": headers_dict,
+    "query_params": query_params_dict
+ }
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request, exc):
+ if exc.status_code == 404:
+    return FileResponse('frontEnd/index.html')
+ else:
+    raise exc
+ 
